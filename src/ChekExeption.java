aaf2d2e6b9170932f563a5b7309f0ff81333e10d
Gudkova Lena <gudkova_elena1992@mail.ru>
2023-01-26 08:52:38 +0300

    total commit

--- src/ChekExeption.java
+++ src/ChekExeption.java
@@ -1,6 +1,5 @@
  class ChekExeption extends Exception{
  ChekExeption(String str){
-    //super("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
     super(str);
 }
 }
